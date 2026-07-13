e6cca26ce643a67231cc3d2af688fe0c57ed5502
urwah7458-tech <urwah7458@gmail.com>
2026-07-13 13:07:17 +0500

    Delete migration.sql

--- migration.sql
+++
@@ -1,3 +0,0 @@
--- Custom SQL migration file, put your code below! --
-INSERT INTO "page_content" ("page_key", "title", "description", "services_json") VALUES
-('home', 'Est. 2014', 'Karachi', '[]');
