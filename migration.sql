5ac1016aa8c087165ff73e0e351b58c39dfc4b51
urwah7458-tech <urwah7458@gmail.com>
2026-07-13 13:09:33 +0500

    Add files via upload

---
+++ migration.sql
@@ -0,0 +1,3 @@
+-- Custom SQL migration file, put your code below! --
+INSERT INTO "page_content" ("page_key", "title", "description", "services_json") VALUES
+('home', 'Est. 2014', 'Karachi', '[]');
